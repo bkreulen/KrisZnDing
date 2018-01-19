bcfab839b8964545c03b038623197a5c5cbe730d
Ferdinand Weynschenk <F.L.Weynschenk@student.tudelft.nl>
2018-01-19 01:02:08 +0100

    invisYO

---
+++ Assets/Scripts/invisCollision.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Networking;
+
+public class invisCollision : NetworkBehaviour
+{
+
+    [SerializeField]
+    private Material ghost;
+    private Material tmpMat;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        MeshRenderer target = other.gameObject.GetComponent<MeshRenderer>();
+        if (!other.gameObject.GetComponent<NetworkIdentity>().isLocalPlayer)        {
+            target.enabled = false;
+        }else
+        {            
+            tmpMat = new Material (target.material);
+            target.material = ghost;
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        MeshRenderer target = other.gameObject.GetComponent<MeshRenderer>();
+        if (!other.gameObject.GetComponent<NetworkIdentity>().isLocalPlayer)
+        {
+            target.enabled = true;
+        }
+        else
+        {
+            target.material = tmpMat;
+        }
+    }
+
+    
+}
